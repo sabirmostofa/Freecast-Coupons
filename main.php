0b0f113a38e41bf48a36b80dfa7ddd66821dd8fa
amira <amira@amira-pc.(none)>
2011-11-07 23:44:42 +0600

    working

--- main.php
+++ main.php
@@ -19,20 +19,27 @@ class wpFreecastCoupons{
     public $single_coupons = array();
 
     function __construct() {
-        $this -> ar= array_merge(range(1,9), range('A','Z'));
+        $this -> ar= array_merge(range(1,9), range('A','N'),range('P','Z'));
         $this -> prevs = (get_option('freecast_coupon_lots'))?get_option('freecast_coupon_lots'):array(); 
         add_action('admin_menu', array($this, 'CreateMenu'), 50);
     }
     
-    function generate_coupons($amt, $val){
+    function generate_coupons($amt){
        $uniq_id = $this -> get_uniq_id();
-       $date = date('Y-m-d H:i:s');
-     
+       $date = date('Y-m-d H:i:s');       
+       $coupons = $this ->generate_single_coupons($amt,$uniq_id);
+       $option_ar = array($uniq_id, $date,$amt,0,$coupons);
+       $prev_rec = get_option('freecast_coupon_lots');
+       if(!$prev_rec)$prev_rec= array();
+      $prev_rec[]=$option_ar;
+      update_option('freecast_coupon_lots', $prev_rec);
+      return $prev_rec;
+      
     }
     function get_uniq_id(){                     
-        $lot_id = $this -> ar[rand(0,34)];
-        $lot_id .= $this -> ar[rand(0,34)];
-        $lot_id .= $this -> ar[rand(0,34)];
+        $lot_id = $this -> ar[rand(0,33)];
+        $lot_id .= $this -> ar[rand(0,33)];
+        $lot_id .= $this -> ar[rand(0,33)];
         if($this -> id_exists($lot_id))return $this ->get_uniq_id();        
        return $lot_id;     
     }
@@ -43,19 +50,21 @@ class wpFreecastCoupons{
         
     }
     
-    function generate_single_coupon(){
-        $coup_id = $this -> ar[rand(0,34)];
-        $coup_id .= $this -> ar[rand(0,34)];
-        $coup_id .= $this -> ar[rand(0,34)];
-        $coup_id .= $this -> ar[rand(0,34)];
-        
-        
-        
+    function generate_single_coupons($amt,$uniq_id){
+        $coupons = array();
+        $counter=0;
+     for($i=0;$i<34;$i++)
+        for($j=0;$j<34;$j++)
+            for($k=0;$k<34;$k++)
+                for($m=0;$m<34;$m++){
+                    $coupons[] = $uniq_id.'-'.$this -> ar[$i].$this -> ar[$j].$this -> ar[$k].$this -> ar[$m];
+                    if(++$counter == $amt )return $coupons;
+                }
+                
+                return  $coupons;  
     }
     
-    function coupon_exists(){
-        if(in_array($coupon, $this -> single_coupons))return true;
-    }
+
 
     function CreateMenu() {
         add_submenu_page('options-general.php', 'Coupon Settings', 'Coupon Settings', 'activate_plugins', 'wpFreecastCoupons', array($this, 'OptionsPage'));
--- options-page.php
+++ options-page.php
@@ -1,11 +1,11 @@
 <?php
-$theads=array('Coupon Lot ID', 'Generation Date', 'Amount', 'Discount', 'Used Amount');
+$theads=array('Coupon Lot ID', 'Generation Date', 'Amount', 'Used Amount');
 global $wpFreecastCoupons;
 if(isset($_POST['main-submit'])):
 	$_POST = array_map( create_function('$a', 'return trim($a);'), $_POST);
 	extract($_POST);        
-        $coupons = $wpFreecastCoupons -> generate_coupons($coupon_amt, $coupon_val);
-        
+        $coupons = $wpFreecastCoupons -> generate_coupons($coupon_amt);
+        if($coupons)echo'Coupons Generated Successfully';
         endif;
      
 ?>
@@ -20,11 +20,6 @@ if(isset($_POST['main-submit'])):
  <br/>
  <br/>
 
- Discount Amount(Default is 40%)
-   <br/>
- <input style="width:40%" type='text' name='coupon_val' value="40"/>
- <br/>
- <br/>
 
   <input class='button-primary' type='submit' name="main-submit" value='Generate Coupons'/> 
     </form>
@@ -43,6 +38,12 @@ if(isset($_POST['main-submit'])):
             </tr>
         </thead>
         <tbody>
+            <?php
+            $prevs = (get_option('freecast_coupon_lots'))?get_option('freecast_coupon_lots'):array();
+            foreach($prevs as $single):
+                echo "<tr><td>$single[0]</td><td>$single[1]</td><td>$single[2]</td><td>$single[3]</td></tr>";
+            endforeach;
+            ?>
         </tbody>
     </table>
 </div>
